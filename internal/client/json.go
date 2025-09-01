1723318ca42b8c0e3d1fa104422a7ad31cfd7ea3
Roland Christen <roland.christen@jaywalker.ch>
2025-09-01 22:04:35 +0200

    json dual types

--- internal/client/json.go
+++ internal/client/json.go
@@ -3,7 +3,10 @@ package client
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
+	"strconv"
+	"strings"
 )
 
 func ParseArrayList(raw []byte) ([]byte, error) {
@@ -30,4 +33,61 @@ func ParseArrayList(raw []byte) ([]byte, error) {
 		return nil, err
 	}
 	return merged, nil
+}
+
+type JsonBool bool
+
+func (sb *JsonBool) UnmarshalJSON(data []byte) error {
+
+	// try boolean
+	var b bool
+	if err := json.Unmarshal(data, &b); err == nil {
+		*sb = JsonBool(b)
+		return nil
+	}
+
+	// try string
+	var s string
+	if err := json.Unmarshal(data, &s); err == nil {
+		val, err := strconv.ParseBool(strings.ToLower(s))
+		if err != nil {
+			return fmt.Errorf("invalid boolean string: %q", s)
+		}
+		*sb = JsonBool(val)
+		return nil
+	}
+
+	return fmt.Errorf("invalid value for JsonBool: %s", string(data))
+}
+
+func (sb *JsonBool) Bool() bool {
+	return bool(*sb)
+}
+
+type JsonInt64 int64
+
+func (si *JsonInt64) UnmarshalJSON(data []byte) error {
+	// try int64
+	var i int64
+	if err := json.Unmarshal(data, &i); err == nil {
+		*si = JsonInt64(i)
+		return nil
+	}
+
+	// try string
+	var s string
+	if err := json.Unmarshal(data, &s); err == nil {
+		val, err := strconv.ParseInt(strings.TrimSpace(s), 10, 64)
+		if err != nil {
+			return fmt.Errorf("invalid int64 string: %q", s)
+		}
+		*si = JsonInt64(val)
+		return nil
+	}
+
+	return fmt.Errorf("invalid value for JsonInt64: %s", string(data))
+}
+
+func (si *JsonInt64) Int64() int64 {
+	return int64(*si)
 }
--- internal/client/json_test.go
+++ internal/client/json_test.go
@@ -37,3 +37,97 @@ func TestJsonArrayParsing(t *testing.T) {
 	assert.Equal(t, "v8", data[3].K2)
 
 }
+
+func TestJsonBool(t *testing.T) {
+
+	{
+		raw := []byte(`true`)
+		var b JsonBool
+		err := json.Unmarshal(raw, &b)
+		assert.NoError(t, err)
+		assert.Equal(t, JsonBool(true), b)
+	}
+	{
+		raw := []byte(`false`)
+		var b JsonBool
+		err := json.Unmarshal(raw, &b)
+		assert.NoError(t, err)
+		assert.Equal(t, JsonBool(false), b)
+
+	}
+	{
+		raw := []byte(`"true"`)
+		var b JsonBool
+		err := json.Unmarshal(raw, &b)
+		assert.NoError(t, err)
+		assert.Equal(t, JsonBool(true), b)
+
+	}
+	{
+		raw := []byte(`"false"`)
+		var b JsonBool
+		err := json.Unmarshal(raw, &b)
+		assert.NoError(t, err)
+		assert.Equal(t, JsonBool(false), b)
+
+	}
+	{
+		raw := []byte(`"TRUE"`)
+		var b JsonBool
+		err := json.Unmarshal(raw, &b)
+		assert.NoError(t, err)
+		assert.Equal(t, JsonBool(true), b)
+
+	}
+	{
+		raw := []byte(`"FALSE"`)
+		var b JsonBool
+		err := json.Unmarshal(raw, &b)
+		assert.NoError(t, err)
+		assert.Equal(t, JsonBool(false), b)
+	}
+	{
+		raw := []byte(`"invalid"`)
+		var b JsonBool
+		err := json.Unmarshal(raw, &b)
+		assert.Error(t, err)
+	}
+	{
+		raw := []byte(`123`)
+		var b JsonBool
+		err := json.Unmarshal(raw, &b)
+		assert.Error(t, err)
+	}
+
+}
+
+func TestJsonInt64(t *testing.T) {
+
+	{
+		raw := []byte(`12345`)
+		var i JsonInt64
+		err := json.Unmarshal(raw, &i)
+		assert.NoError(t, err)
+		assert.Equal(t, JsonInt64(12345), i)
+	}
+	{
+		raw := []byte(`"12345"`)
+		var i JsonInt64
+		err := json.Unmarshal(raw, &i)
+		assert.NoError(t, err)
+		assert.Equal(t, JsonInt64(12345), i)
+	}
+	{
+		raw := []byte(`"  67890  "`)
+		var i JsonInt64
+		err := json.Unmarshal(raw, &i)
+		assert.NoError(t, err)
+		assert.Equal(t, JsonInt64(67890), i)
+	}
+	{
+		raw := []byte(`"invalid"`)
+		var i JsonInt64
+		err := json.Unmarshal(raw, &i)
+		assert.Error(t, err)
+	}
+}
